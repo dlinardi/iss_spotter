5b72e8138c7409794aeb2165251c78bdf0604a57
dlinardi <dlinardi98@gmail.com>
2020-11-06 05:45:38 +0000

    refactored and linted code

--- iss.js
+++ iss.js
@@ -1,14 +1,5 @@
 const request = require('request');
 
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
-
 const fetchMyIP = (callback) => {
   request('https://api.ipify.org/?format=json', (error, response, body) => {
 
@@ -78,25 +69,18 @@ const fetchISSFlyOverTimes = (coords, callback) => {
 
 const nextISSTimesForMyLocation = (callback) => {
   fetchMyIP((error, ip) => {
-
     if (error) {
       return callback(error, null);
     }
-
     fetchCoordsByIP(ip, (error, coords) => {
-
       if (error) {
         return callback(error, null);
       }
-
       fetchISSFlyOverTimes(coords, (error, nextPasses) => {
-
         if (error) {
           return callback(error, null);
         }
-
         callback(null, nextPasses);
-
       });
     });
   });
